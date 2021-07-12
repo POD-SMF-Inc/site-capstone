131735de30d0cebea4dc6ac2266f2e09cd0fbd89
fdhernandez <rodriguezhernandez0406@gmail.com>
2021-07-12 14:01:11 -0700

    frontend login/register

--- site_capstone_ui/src/components/App/App.js
+++ site_capstone_ui/src/components/App/App.js
@@ -1,24 +1,63 @@
-
 import './App.css';
+import { useEffect, useState} from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import API from '../../services/apiClient'
 
-function App() {
+export default function App() {
+  const [appState, setAppState] = useState({})
+  const [errors, setErrors] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  
+  
+
+  
+  const handleLogout = async () => {
+    await API.logoutUser()
+    setAppState({})
+    console.log("app", appState)
+    setErrors(null)
+  }
+
+    /** Fetch user by token generated */
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true)
+      console.log("refresh")
+      const { data } = await API.fetchUserFromToken()
+      if (data) {
+        setAppState((a) => ({...a, user: data.user}))
+      }
+      setIsLoading(false)
+    }
+
+    // only set token if it exists
+    const token = localStorage.getItem("tracker_token")
+    if (token) {
+      API.setToken(token)
+      fetchUser()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {!isLoading ? 
+        <>
+        
+        <Routes>
+          
+          <Route path='/register' element={ <Register  setAppState={setAppState}/>} />
+          <Route path='/login' element={ <Login  setAppState={setAppState}/>} />
+
+        
+        </Routes>
+        </> : null }
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
